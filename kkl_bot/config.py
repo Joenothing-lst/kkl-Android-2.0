f1ecac03add14225adddfe2c9d24fa3d79b21b7d
Joenothing-lst <56671035+Joenothing-lst@users.noreply.github.com>
2019-12-04 20:05:24 +0800

    Update config.py

--- kkl_bot/config.py
+++ kkl_bot/config.py
@@ -3,5 +3,5 @@ from nonebot.default_config import *
 
 COMMAND_START = {'/', '!', '／', '！',''}
 NICKNAME = {'妈','可可萝'}
-MASTER = [912871833]
-JJC_KEY=''
+MASTER = [912871833]     #从高到低排序
+JJC_KEY=''               #用于jjc查询
