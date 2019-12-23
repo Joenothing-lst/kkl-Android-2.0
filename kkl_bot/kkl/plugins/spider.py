f3e0c6426072ff23363dcc15efc17d3b09810cdc
Joenothing-lst <56671035+Joenothing-lst@users.noreply.github.com>
2019-12-23 17:48:26 +0800

    Update spider.py

--- kkl_bot/kkl/plugins/spider.py
+++ kkl_bot/kkl/plugins/spider.py
@@ -3,13 +3,25 @@ import nonebot
 from nonebot import on_command, CommandSession, permission as perm
 import os
 import re
-import requests
 import jjcsearch
 from random import randint
 from google_translate import translate, ref_words
+from aiohttp import ClientSession
 import datetime as dt
 import jianfan
 import json
+
+async def get_bytes(url, headers=None):
+    async with ClientSession() as asyncsession:
+        async with asyncsession.get(url,headers=headers) as response:
+            b = await response.read()
+    return b
+
+async def post_bytes(url, headers=None,data=None):
+    async with ClientSession() as asyncsession:
+        async with asyncsession.post(url,headers=headers,data=data) as response:
+            b = await response.read()
+    return b
 
 new_switch,hbook_switch,jjcsearch_switch=True,True,True
 @on_command('switch', aliases=('开启','关闭'), only_to_me=False) 
@@ -45,9 +57,10 @@ async def switch(session: CommandSession):
 @on_command('Rnews', aliases=('日服新闻','日服活动'), only_to_me=True)
 async def Rnews(session: CommandSession):
     header = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
-
     url = 'https://priconne-redive.jp/news/'
-    data = requests.get(url,headers = header)
+    
+    data = await get_bytes(url,headers = header)
+    data = data.decode()
 
     pattern_title = '<h4>(.*?)</h4>'#标题
     title = re.findall(pattern_title,data.text)
@@ -70,7 +83,9 @@ async def Rnews(session: CommandSession):
 async def Tnews(session: CommandSession):
     url = 'http://www.princessconnect.so-net.tw/news'
     header ={ 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
-    data = requests.get(url,headers = header).text
+    
+    data = await get_bytes(url,headers = header)
+    data = data.decode()
     #标题
     pattern_title = '<a href="/news/newsDetail/.*?">(.*?)</a>'#标题
     title = re.findall(pattern_title,data)
@@ -91,8 +106,11 @@ async def Tnews(session: CommandSession):
 @on_command('Tevents', aliases=('台服活动',), only_to_me=True)
 async def Tevents(session: CommandSession):
     header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
-    response = requests.get('https://pcredivewiki.tw/static/data/event.json',headers = header)
-    data = json.loads(response.text)
+    url = 'https://pcredivewiki.tw/static/data/event.json'
+
+    data = await get_bytes(url,headers = header)
+    data = json.loads(data.decode())
+
     n=dt.datetime.now()
     msg0 = '已查询到以下活动：'
     for i in data:
@@ -106,8 +124,12 @@ async def Tevents(session: CommandSession):
 
 @on_command('jjcsearch', aliases=('jjc查询','JJC查询','怎么拆','怎么解'), only_to_me=False)                 #changed
 async def jjcs(session: CommandSession):
-    if ' ' in session.ctx['raw_message'] and jjcsearch_switch: 
-        result = jjcsearch.total(session.ctx['raw_message'],session.ctx['user_id'],session.bot.config.JJC_KEY)
+    msg=session.current_arg.strip()
+    if jjcsearch_switch: 
+        msg=session.current_arg.strip()
+        if not msg:
+            f_msg = session.get('message', prompt='骑士君想查什么阵容呢？\nps：用空格分隔')
+        result = jjcsearch.total(msg,session.ctx['user_id'],session.bot.config.JJC_KEY)
         await session.send(result)
 
 @on_command('ja_to_zh', aliases=('日语翻译',), only_to_me=False)                 #changed
@@ -145,19 +167,22 @@ async def ja_to_zh(session: CommandSession):
 
 @on_command('hbook', aliases=('本子查询','找本子'), only_to_me=False)
 async def hbooks(session: CommandSession):
-    if ' ' in session.ctx['raw_message'] and hbook_switch: 
-        f_msg = session.ctx['raw_message'].split(' ',1)[1]#获取关键词
+    if hbook_switch: 
+        f_msg=session.current_arg.strip()
+        if not f_msg:
+            f_msg = session.get('message', prompt='准备完成，请发送关键词')
         f_type = session.ctx['message_type']
         f_qq = session.ctx['user_id']
         header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
         keyword={'show':'title,titleen,tags','keyboard':f_msg}
-        responce = requests.post('https://b-upp.com/search/',headers = header,data = keyword)
-        if '没有搜索到相关的内容' in responce.text:
+        responce = await post_bytes('https://b-upp.com/search/',headers = header,data = keyword)
+        responce = responce.decode()
+        if '没有搜索到相关的内容' in responce:
             n_msg='可可萝没有找到关键词[{}]相关的本子哦'.format(f_msg)
             await session.send(message=n_msg)
         else:
             p = '<a href="(.*?)" target="_blank" title="(.*?)">'
-            data = re.findall(p,responce.text)
+            data = re.findall(p,responce)
             n = len(data)
             if f_type == 'group':
                 limit=3
@@ -176,33 +201,37 @@ async def hbooks(session: CommandSession):
 
 @on_command('anime_search', aliases=('以图搜番','这是什么番','搜番','什么番','番剧查询','这是什么动漫','动漫查询','搜动漫','什么动漫'), only_to_me=False)
 async def anime_search(session: CommandSession):
+    msg=session.current_arg.strip()
+    if not msg:
+        msg = session.get('message', prompt='准备完成，请发送图片')
     p='\\[CQ\\:image\\,file\\=.*?\\,url\\=(.*?)\\]'
-    img=re.findall(p,str(session.ctx['message']))
-    url=f'https://trace.moe/api/search?url={img[0]}'
-    r = requests.get(url)
-    if r.status_code==200:
-        data=json.loads(r.text)
-        d={}
-        for i in range(len(data['docs'])):
-            if data['docs'][i]['title_chinese'] in d.keys():
-                d[data['docs'][i]['title_chinese']][0]+=data['docs'][i]['similarity']
-            else:
-                m=data['docs'][i]['at']/60
-                s=data['docs'][i]['at']%60
-                if data['docs'][i]['episode']=='':
-                    n=1
+    img=re.findall(p,msg)
+    if img:
+        url=f'https://trace.moe/api/search?url={img[0]}'
+        r = await get_bytes(url)
+        if r:
+            data=json.loads(r.decode())
+            d={}
+            for i in range(len(data['docs'])):
+                if data['docs'][i]['title_chinese'] in d.keys():
+                    d[data['docs'][i]['title_chinese']][0]+=data['docs'][i]['similarity']
                 else:
-                    n=data['docs'][i]['episode']
-                d[jianfan.toSimpleString(data['docs'][i]['title_chinese'])]=[data['docs'][i]['similarity'],f'第{n}集',f'{int(m)}分{int(s)}秒处']
-        result=sorted(d.items(),key=lambda x:x[1],reverse=True)
-        t=0
-        msg0=f'为骑士君按相似度找到以下{len(d)}个动漫：'
-        for i in result:
-            t+=1
-            msg=(f'\n-----------------------------------------------\n({t})\n相似度：{i[1][0]}\n动漫名：《{i[0]}》\n时间点：{i[1][1]} {i[1][2]}')
-            msg0+=msg
-        await session.send(msg0)
-    else :
-        print(r)
-        await session.send('在下太忙啦，过会再来问吧')
+                    m=data['docs'][i]['at']/60
+                    s=data['docs'][i]['at']%60
+                    if data['docs'][i]['episode']=='':
+                        n=1
+                    else:
+                        n=data['docs'][i]['episode']
+                    d[jianfan.toSimpleString(data['docs'][i]['title_chinese'])]=[data['docs'][i]['similarity'],f'第{n}集',f'{int(m)}分{int(s)}秒处']
+            result=sorted(d.items(),key=lambda x:x[1],reverse=True)
+            t=0
+            msg0=f'为骑士君按相似度找到以下{len(d)}个动漫：'
+            for i in result:
+                t+=1
+                msg=(f'\n-----------------------------------------------\n({t})\n相似度：{i[1][0]}\n动漫名：《{i[0]}》\n时间点：{i[1][1]} {i[1][2]}')
+                msg0+=msg
+            await session.send(msg0)
+        else :
+            print(r.decode())
+            await session.send('在下太忙啦，过会再来问吧')
 
