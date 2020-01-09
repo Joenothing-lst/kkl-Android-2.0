b7f8dd50b252b7369c2c36abfb941201957da844
Joenothing-lst <56671035+Joenothing-lst@users.noreply.github.com>
2020-01-10 01:07:54 +0800

    Update wenda.py

--- kkl_bot/kkl/plugins/plus/wenda.py
+++ kkl_bot/kkl/plugins/plus/wenda.py
@@ -1,25 +1,25 @@
 # -*- coding:utf-8 -*-
 import os
 import re
-import ast
-root=os.path.join(os.path.dirname(__file__),'kkl_dictionary.txt')
+import json
+root=os.path.join(os.path.dirname(__file__),'kkl_dictionary.json')
 a={1:{}}
 #保存词库方法
-def savedir(dir):
+def savedir(js):
     global a
     with open(root,'w',encoding='utf-8') as file:
-        data=file.write(str(dir))
+        file.write(js)
     return '已保存~'
 #读取词库方法
 def readdir():
     global a
     with open(root,'r',encoding='utf-8') as file:
-        data=file.read()
-        a=ast.literal_eval(data)
+        a=json.load(file)
     return '已读取~'
 #存词条方法
 def add(msg,id):
     global a
+    id=str(id)
     p='问(.*?)答(.+)'
     msg=re.findall(p,msg)[0]
     if id in a:
@@ -31,9 +31,10 @@ def add(msg,id):
 #删除词条方法
 def delet(msg,id):
     global a
+    id=str(id)
     msg=re.findall('词条(.+)',msg)[0]
     try:
-        if (msg in [i for i in a[1]]) and (msg not in [i for i in a[id]]):
+        if (msg in [i for i in a['1']]) and (msg not in [i for i in a[id]]):
             return '你没有权限修改全局词条哦~'
         del a[id][msg]
         savedir(a)
@@ -42,6 +43,7 @@ def delet(msg,id):
         return '可可萝不知道这个词哦~'
 #响应方法
 def reply(msg,id):
+    id=str(id)
     if '删除' not in msg:
         #判断有无词典,优先级高于全局
         if id in a:
@@ -49,6 +51,6 @@ def reply(msg,id):
                 if i in msg:
                     return a[id][i]
         #判断是否全局
-        for i in a[1]:
+        for i in a['1']:
             if i in msg:
-                return a[1][i]
+                return a['1'][i]
